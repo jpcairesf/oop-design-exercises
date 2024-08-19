e63462c09c71426f195aca98fc3c69ab2e3e9f0a
jpcairesf <jpcairesf@gmail.com>
2024-08-19 20:59:58 -0300

    feat: Library OOP exercise, tests and uml

---
+++ library/library.py
@@ -0,0 +1,62 @@
+import collections
+
+
+class Book:
+    def __init__(self, isbn, title, author):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.available = True
+
+
+class Member:
+    def __init__(self, name, member_id):
+        self.name = name
+        self.member_id = member_id
+
+
+class Library:
+    def __init__(self):
+        self.borrowed = collections.defaultdict(list)
+        self.members = {}
+        self.books = {}
+
+    def add_book(self, isbn, title, author):
+        self.books[isbn] = Book(isbn, title, author)
+
+    def create_member(self, name, member_id):
+        self.members[member_id] = Member(name, member_id)
+
+    def lend_book(self, member_id, isbn):
+        self._validate_member(member_id)
+        book = self._find_book_by_isbn(isbn)
+        if not book.available:
+            raise ValueError("Book not available")
+        book.available = False
+        self.borrowed[member_id].append(book)
+
+    def return_book(self, member_id, isbn):
+        self._validate_member(member_id)
+        book = self._find_book_by_isbn(isbn)
+        if book not in self.borrowed[member_id]:
+            raise ValueError("The member did not borrow this book")
+        book.available = True
+        self.borrowed[member_id].remove(book)
+
+    def check_availability(self, isbn):
+        if isbn in self.books:
+            return self.books[isbn].available
+        raise ValueError("Book not found")
+
+    def get_borrowed_books(self, member_id):
+        self._validate_member(member_id)
+        return [book.isbn for book in self.borrowed[member_id]]
+
+    def _validate_member(self, member_id):
+        if member_id not in self.members:
+            raise ValueError("Member not found")
+
+    def _find_book_by_isbn(self, isbn):
+        if isbn in self.books:
+            return self.books[isbn]
+        raise ValueError("Book not found")
---
+++ library/test/test_library.py
@@ -0,0 +1,48 @@
+import unittest
+
+from library.library import Library
+
+
+class Tests(unittest.TestCase):
+    def setUp(self):
+        self.library = Library()
+
+    def test_register_book(self):
+        self.library.add_book(0, "John's Adventures", "Johnny")
+        self.assertIn(0, self.library.books)
+        self.library.add_book(1, "Jane's Chronicles", "Janette")
+        self.assertIn(1, self.library.books)
+
+    def test_register_member(self):
+        self.library.create_member("John", 0)
+        self.assertIn(0, self.library.members)
+        self.library.create_member("Jane", 1)
+        self.assertIn(1, self.library.members)
+
+    def test_check_availability(self):
+        self.library.create_member("John", 0)
+        self.library.add_book(0, "John's Adventures", "Johnny")
+        self.assertTrue(self.library.check_availability(0))
+        self.library.lend_book(0, 0)
+        self.assertFalse(self.library.check_availability(0))
+
+    def test_lend_a_book(self):
+        self.library.create_member("John", 0)
+        self.library.add_book(0, "John's Adventures", "Johnny")
+        self.library.lend_book(0, 0)
+        self.assertIn(0, [book.isbn for book in self.library.borrowed[0]])
+
+    def test_return_a_book(self):
+        self.library.create_member("John", 0)
+        self.library.add_book(0, "John's Adventures", "Johnny")
+        self.library.lend_book(0, 0)
+        self.library.return_book(0, 0)
+        self.assertNotIn(0, [book.isbn for book in self.library.borrowed[0]])
+
+    def test_get_borrowed_books(self):
+        self.library.create_member("John", 0)
+        self.library.add_book(0, "John's Adventures", "Johnny")
+        self.library.add_book(1, "Jane's Chronicles", "Janette")
+        self.library.lend_book(0, 0)
+        self.library.lend_book(0, 1)
+        self.assertAlmostEqual(2, len(self.library.get_borrowed_books(0)))
